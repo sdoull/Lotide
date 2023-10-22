0157060c8c70e4df6b8c4b0904daf500840a826a
Stephanie <stephaniedoull@hotmail.com>
2023-10-22 11:10:20 -0300

    cleaned up code

--- assertEqual.js
+++ assertEqual.js
@@ -24,11 +24,3 @@ assertEqual(62, 102);
 
 
 
-// // If the values match, it should print (console.log)
-//  the following: Assertion Passed:
-//   [actual] === [expected]
-//   (but with the values filled in)
-// // Otherwise it should print (console.log)
-//  the following: Assertion Failed:
-//   [actual] !== [expected]
-//    (but with the values filled in)
