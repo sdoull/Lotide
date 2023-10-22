fe8f1923aed11e3c4f127c137024fa874408a4bf
Stephanie <stephaniedoull@hotmail.com>
2023-10-22 10:46:48 -0300

    asserEqual completed code with emoji

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,32 @@
-const assertEqual = function(actual, expected){
+const passed = "😍";
+const failed = "🤬";
 
-
+const assertEqual = function(actual, expected) {
+  if (actual === expected)   {
+    console.log(`${passed}Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failed}Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+
+
+
+
+
+
+
+
+
+
+// // If the values match, it should print (console.log)
+//  the following: Assertion Passed:
+//   [actual] === [expected]
+//   (but with the values filled in)
+// // Otherwise it should print (console.log)
+//  the following: Assertion Failed:
+//   [actual] !== [expected]
+//    (but with the values filled in)
