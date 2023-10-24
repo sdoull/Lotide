dc7be8c6e11ca17d6b0b63c9aa95e359fd251baa
Stephanie <stephaniedoull@hotmail.com>
2023-10-23 22:38:01 -0300

    Loopy Lighthouse Refactor

---
+++ loopy-lightHouse-refactor.js
@@ -0,0 +1,12 @@
+for (let i = 100; i <= 200; i++) {
+  let divisibleByThree = i % 3 === 0;
+  let divisibleByFour = i % 4 === 0;
+    
+  console.log(
+    divisibleByThree && divisibleByFour 
+      ? "LoopyLighthouse" 
+      : divisibleByThree 
+        ? "Loopy" 
+        : divisibleByFour 
+          ? "Lighthouse" : i);
+}
