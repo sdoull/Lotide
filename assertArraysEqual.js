bfdbd7a010b89d349cc214fb2b259759c7321709
Stephanie <stephaniedoull@hotmail.com>
2023-10-26 17:59:39 -0300

    AI feedback change

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -36,3 +36,4 @@ assertArraysEqual([1, 2, 3], [1, 2, 3]); // Should pass
 assertArraysEqual([1, 2, 3], [3, 2, 1]); // Should fail
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // Should pass
 assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // Should fail
+// console.log(`${passed} Assertion Passed: ${array1} === ${array2}`);
