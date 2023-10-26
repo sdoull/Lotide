5eb91f4f0060c437f9bc75d0eb9dd95ad807e4bd
Stephanie <stephaniedoull@hotmail.com>
2023-10-26 17:45:42 -0300

    eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,43 @@
+const passed = "😍";
+const failed = "🤬";
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected)   {
+    console.log(`${passed}Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failed}Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual(62, 102);
+
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  
+  return true;
+};
+  
+// Test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays([1], [1]), true);
+assertEqual(eqArrays([1], [2]), false);
+assertEqual(eqArrays(["apple", "banana", "cherry"], ["apple", "banana", "cherry"]), true);
+assertEqual(eqArrays(["apple", "banana", "cherry"], ["apple", "banana", "kiwi"]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
