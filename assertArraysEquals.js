29f52ff496bcdacb7210f5feac73e6080969eb8d
Stephanie <stephaniedoull@hotmail.com>
2023-10-26 17:53:06 -0300

    assertArraysEquals.js

---
+++ assertArraysEquals.js
@@ -0,0 +1,38 @@
+const passed = "😍";
+const failed = "🤬";
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${passed} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failed} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`${passed} Assertion Passed: Arrays are equal.`);
+  } else {
+    console.log(`${failed} Assertion Failed: Arrays are not equal.`);
+  }
+};
+
+// Test cases (You can visually inspect the console output)
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // Should pass
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // Should fail
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // Should pass
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // Should fail
